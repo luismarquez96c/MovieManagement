fca714070e91a24d9e9b7fd2f2629c2a77c9b971
luismarquez96c <luismarquez96c@gmail.com>
2023-11-30 16:23:41 -0600

    Logro: Crear proyecto movie management

---
+++ src/main/java/net/luismarquez/projects/MovieManagement/MovieManagementApplication.java
@@ -0,0 +1,13 @@
+package net.luismarquez.projects.MovieManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagementApplication.class, args);
+	}
+
+}
